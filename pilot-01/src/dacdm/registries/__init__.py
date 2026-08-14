de512e499e33d835753f590be0baec81ae4d41b0
skys0-0 <sun0_02@yahoo.com.hk>
2026-08-14 20:47:15 +0800

    Export S1.3 historical snapshot schema

--- pilot-01/src/dacdm/registries/__init__.py
+++ pilot-01/src/dacdm/registries/__init__.py
@@ -1,6 +1,7 @@
 """Registry schemas and validation for DACDM Pilot 01."""
 
 from .models import (
+    HistoricalSnapshotEvidenceRecord,
     ModelRegistryRecord,
     PricingEvidenceRecord,
     TaskRegistryRecord,
@@ -9,6 +10,7 @@ from .models import (
 from .validator import validate_registry_files
 
 __all__ = [
+    "HistoricalSnapshotEvidenceRecord",
     "ModelRegistryRecord",
     "PricingEvidenceRecord",
     "TaskRegistryRecord",
